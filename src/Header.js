36846bb237b0619e0b8f8651a20e0db4214dc433
Ksanha <sanha142@gmail.com>
2022-10-26 16:47:40 +0900

    comment : 필요한 주석 추가 및 변경

--- src/Header.js
+++ src/Header.js
@@ -1,14 +1,21 @@
 import React from "react";
 import "./Header.css";
-import { Link } from 'react-router-dom';
+import {
+	 BrowserRouter as router,
+	 Switch,
+	 Router,
+	 Link
+	} from 'react-router-dom';
+
+
 
 function Header() {
   return (
     <div id="wrapper">
 		<header id="leftContents">
 			<nav>
-				<a href="login.jsp"><li>로그인</li></a>
-				{/* <Link to="경로">링크명</Link> */}
+				<a href="./login.jsp"><li>로그인</li></a>
+				{/* <Link to = "/login">로그인</Link> */}
 				<a href="signUp.jsp"><li>회원가입</li></a>
 			</nav>
 			<ul id="topMenu">
